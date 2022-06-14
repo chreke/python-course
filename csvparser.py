2ea308152d76ae88c2cc4b8284fc3a31d7044806
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-06-14 15:43:29 +0200

    Refactor CSV parser

--- csvparser.py
+++ csvparser.py
@@ -1,10 +1,5 @@
-def parse_csv(filename):
-    rows = []
-    with open(filename, "r") as f:
-        headers = next(f).strip().split(",")
-        for line in f:
-            row = line.strip().split(",")
-            rows.append(
-                dict(zip(headers, row))
-            )
-    return rows
+def parse_csv(iterable):
+    headers = next(iterable).strip().split(",")
+    for line in iterable:
+        row = line.strip().split(",")
+        yield dict(zip(headers, row))
